d09e327cdd97253df4e2180cf5fc781656de43d4
TrxMod21X <7teukurizky@gmail.com>
2022-12-06 22:02:52 +0700

    Adding Login and Register Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import GlobalStyles from "./components/GlobalStyles";
 import Calculators from "./components/Calculators";
+// import Headers from "./components/Headers";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Calculators />
+      {/* <Headers /> */}
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/register" element={<SignupForm />} />
+        <Route path="/calculator" element={<Calculators />} />
+      </Routes>
     </>
   );
 }
