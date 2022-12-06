870112d71796e3abae8c681c3b7b22daefaec37b
TrxMod21X <7teukurizky@gmail.com>
2022-12-06 17:14:43 +0700

    Adding Calculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "./components/GlobalStyles";
+import Calculators from "./components/Calculators";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Calculators />
+    </>
+  );
+}
+
+export default App;
